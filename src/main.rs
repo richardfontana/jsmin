50d70cf8476c49efa33ee1d0fb3665f0272efb56
Richard Fontana <fontana@sharpeleven.org>
2015-05-05 23:17:59 -0400

    Add file i/o to main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::error::Error;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let arg = env::args().nth(1);
+  
+    match arg {
+        Some(s) => {
+            let path = Path::new(&s);
+            let display = path.display();
+            let mut file = match File::open(&path) {
+                Err(why) => panic!("couldn't open {}: {}", display,
+                                   Error::description(&why)),
+                Ok(file) => file,
+            };
+            let mut buf = String::new();
+            match file.read_to_string(&mut buf) {
+                Err(why) => panic!("couldn't read {}: {}", display,
+                                   Error::description(&why)),
+                Ok(_)    => print!("{} contains\n{}", display, buf),
+            }
+        },
+        None    => println!("Usage: jsmin <FILENAME>"),
+    }
 }
+
